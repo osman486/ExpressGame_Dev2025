365487da78903545b1c32910e4bbab6a2a7238ef
Osman_ibragim <osmanIbrag@gmail.com>
2025-12-29 12:32:39 +0300

    Пункт 8_3

--- routes/game_devs.js
+++ routes/game_devs.js
@@ -1,13 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var Game_Dev = require("../models/game_dev.js").Game_Dev;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с game_devs');
+router.get("/", function (req, res, next) {
+  res.send("Новый маршрутизатор, для маршрутов, начинающихся с game_devs");
 });
 
-router.get("/:nick", function(req, res, next) {
-   res.send(req.params.nick);
+router.get("/:nick", async function (req, res, next) {
+  var game_devs = await Game_Dev.find({ nick: req.params.nick });
+
+  console.log(game_devs);
+
+  if (!game_devs.length)
+    return  res.send("Нет такого вида гейм дева");
+
+  var game_dev = game_devs[0];
+
+  res.render("game_dev", {
+    title: game_dev.title,
+    picture: game_dev.avatar,
+    desc: game_dev.desc,
+  });
 });
 
 module.exports = router;
