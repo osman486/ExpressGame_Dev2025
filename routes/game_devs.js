a2683ef7ffbce8a04df752087bcf1946923e4abd
Osman_ibragim <osmanIbrag@gmail.com>
2025-12-29 16:10:45 +0300

    Пункт 10_8

--- routes/game_devs.js
+++ routes/game_devs.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var Game_Dev = require("../models/game_dev.js").Game_Dev;
-
+var checkAuth = require("../middlewares/checkAuth.js");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("Новый маршрутизатор, для маршрутов, начинающихся с game_devs");
 });
 
-router.get("/:nick", async function (req, res, next) {
+router.get("/:nick", checkAuth , async function(req, res, next) {
   var game_devs = await Game_Dev.find({ nick: req.params.nick });
 
   console.log(game_devs);
