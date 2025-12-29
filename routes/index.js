77cafc7a3407f6250c273594708e0c15e9f935cb
Osman_ibragim <osmanIbrag@gmail.com>
2025-12-29 15:00:52 +0300

    Пункт 10_1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express',  counter:req.session.counter });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express", counter: req.session.counter });
 });
 
+router.get("/logreg", function (req, res, next) {
+  res.render("logreg", { title: "Вход" });
+});
 
 module.exports = router;
