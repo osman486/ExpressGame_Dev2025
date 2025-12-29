d0906b342eaf27a2a55e43019addd3f6fd77db71
Osman_ibragim <osmanIbrag@gmail.com>
2025-12-29 15:32:18 +0300

    Пункт 10_4

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
 var mongoose = require("mongoose");
-var crypto = require("crypto")
+var crypto = require("crypto");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   username: {
     type: String,
-    
     unique: true,
     required: true,
   },
@@ -26,16 +25,21 @@ var userSchema = new Schema({
   },
 });
 
-userSchema.virtual("password").set(function(password){
-   this._purePassword = password
-   this.salt = Math.random() + ""
-   this.hashedPassword = this.encryptPassword(password)
-}).get(function(){
-   return this._purePassword
-})
+userSchema.virtual("password").set(function (password) {
+    this._purePassword = password;
+    this.salt = Math.random() + "";
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._purePassword;
+  });
 
-userSchema.methods.encryptPassword = function(password){
-   return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
-}
+userSchema.methods.encryptPassword = function (password) {
+  return crypto.createHmac("sha1", this.salt).update(password).digest("hex");
+};
+
+userSchema.methods.checkPassword = function (password) {
+  return this.encryptPassword(password) === this.hashedPassword;
+};
 
 module.exports.User = mongoose.model("User", userSchema);
