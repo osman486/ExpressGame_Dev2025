336552abc538e29ce6fe46437601acaad71468cb
Osman_ibragim <osmanIbrag@gmail.com>
2025-12-29 11:53:34 +0300

    Пункт 8_2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/game_dev2025')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexGame_Dev = require('./routes/game_devs');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/game_devs', indexGame_Dev);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
